f5eee18c2a97863f75008c250c1a9ad20d5a827c
Rob Whitaker <dev@robwhitaker.com>
2021-05-17 23:52:21 -0400

    Add basic player mechanics
    * Movement
        * Walk
        * Ctrl to "drag body" (until actual body dragging implemented)
        * Shift to Sprint
        * Moving faster increases your detection radius for enemies
    * Interactions
        * Player now has an interaction collider (Area2D)
        * Can interact with anything that has a `select` and `interact`
        function with E
    * Lantern
        * Player illuminated by a default (but very dim) light when lantern
        is off
        * Pressing F toggles the lantern, which is much brighter but greatly
        increases your detection radius

--- scripts/characters/Player.gd
+++ scripts/characters/Player.gd
@@ -1,31 +1,49 @@
 extends KinematicBody2D
 
 const ACCELERATION = 500
-const MAX_SPEED = 80
 const FRICTION = 500
+
+const DRAG_MAX_SPEED = 30
+const WALK_MAX_SPEED = 65
+const SPRINT_MAX_SPEED = 120
+
+const WALK_DETECTION_MULT = 3
+const SPRINT_DETECTION_MULT = 8
+const LIGHT_DETECTION_MULT = 12
+
+enum {
+    DRAG,
+    WALK,
+    SPRINT
+}
 
 enum {
     MOVE,
-    ROLL,
     ATTACK
 }
 
 var state = MOVE
+var move_state = WALK
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var detection_radius_shape := $DetectionRadius/CollisionShape2D as CollisionShape2D
+onready var lantern_light := $LanternLight as Light2D
+onready var default_light := $DefaultLight as Light2D
+
+# Node we currently have selected for interaction
+var selected_interaction = null
 
 func _ready():
+
     animationTree.active = true
 
 func _process(delta):
     match state:
         MOVE:
             move_state(delta)
-        ROLL:
-            pass
         ATTACK:
             attack_state()
 
@@ -35,15 +53,35 @@ func move_state(delta):
     input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
     input_vector = input_vector.normalized()
 
+    if Input.is_action_pressed("sprint"):
+        move_state = SPRINT
+        detection_radius_shape.set_scale(Vector2(SPRINT_DETECTION_MULT, SPRINT_DETECTION_MULT))
+    elif Input.is_action_pressed("drag"):
+        move_state = DRAG
+        detection_radius_shape.set_scale(Vector2(WALK_DETECTION_MULT, WALK_DETECTION_MULT))
+    else:
+        move_state = WALK
+        detection_radius_shape.set_scale(Vector2(WALK_DETECTION_MULT, WALK_DETECTION_MULT))
+
     if input_vector != Vector2.ZERO:
         animationTree.set("parameters/Idle/blend_position", input_vector)
         animationTree.set("parameters/Run/blend_position", input_vector)
         animationTree.set("parameters/Attack/blend_position", input_vector)
+        # TODO: different animation for different move states
         animationState.travel("Run")
-        velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+        var max_speed := 0
+        match move_state:
+            DRAG:
+                max_speed = DRAG_MAX_SPEED
+            WALK:
+                max_speed = WALK_MAX_SPEED
+            SPRINT:
+                max_speed = SPRINT_MAX_SPEED
+        velocity = velocity.move_toward(input_vector * max_speed, ACCELERATION * delta)
     else:
         animationState.travel("Idle")
         velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+        detection_radius_shape.set_scale(Vector2(1,1))
 
     velocity = move_and_slide(velocity)
 
@@ -51,8 +89,36 @@ func move_state(delta):
         velocity = Vector2.ZERO
         state = ATTACK
 
+    if lantern_light.is_visible():
+        detection_radius_shape.set_scale(Vector2(LIGHT_DETECTION_MULT, LIGHT_DETECTION_MULT))
+
 func attack_state():
     animationState.travel("Attack")
 
 func attack_animation_finish():
     state = MOVE
+
+func _on_InteractionRadius_area_entered(body) -> void:
+    if body.has_method("select"):
+        if selected_interaction != null && selected_interaction.has_method("unselect"):
+            selected_interaction.unselect()
+        selected_interaction = body
+        body.select()
+
+func _on_InteractionRadius_area_exited(body) -> void:
+    if body == selected_interaction && selected_interaction.has_method("unselect"):
+        selected_interaction.unselect()
+        selected_interaction = null
+
+func _unhandled_input(event):
+    if ( selected_interaction != null
+      && selected_interaction.has_method("interact")
+      && event.is_action_pressed("interact")):
+        selected_interaction.interact()
+    elif event.is_action_pressed("lantern"):
+        if lantern_light.is_visible():
+            lantern_light.hide()
+            default_light.show()
+        else:
+            lantern_light.show()
+            default_light.hide()
